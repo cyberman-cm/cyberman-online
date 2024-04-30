5437ab054d50d9c7383e906b16f1a4d2b63ca4df
Cyber Man <162600878+cyberman-online@users.noreply.github.com>
2024-04-30 21:23:54 +0100

    Add files via upload

---
+++ onnline.js
@@ -0,0 +1,19 @@
+
+
+
+
+window.addEventListener("load", function() {
+    document.querySelector("iframe").style.opacity = 1;
+    document.querySelector(".loading").style.opacity = 0;
+    document.querySelector(".loading").style.display = 'none';
+});
+
+
+
+
+
+
+
+
+
+  
